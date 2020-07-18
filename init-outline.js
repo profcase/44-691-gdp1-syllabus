24dcc9f25e1a64bfa33ef3e672559923c7f84827
Denise Case <denisecase@gmail.com>
2020-07-18 16:03:43 -0500

    add outline; update test results

--- init-outline.js
+++ init-outline.js
@@ -3,7 +3,7 @@
 export default {
   mod1: "Course intro, client requirements and RFPs, rapid response",
   mod2: "Team organization, work allocation, begin consolidated design documentation",
-  mod3: "Begin implementation of Minimially Viable Products (MVP)",
+  mod3: "Begin implementation of Minimally Viable Products (MVP)",
   mod4: "Demonstrations, updates to design and MVPs",
   mod5: "Demonstrations, updates to design and MVPs",
   mod6: "Demonstrations, updates to design and MVPs",
