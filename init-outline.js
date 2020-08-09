b6ac95d091587781bd1aff999b9b42f0625bc5af
Denise Case <denisecase@users.noreply.github.com>
2020-08-09 05:08:55 -0500

    Update init-outline.js

--- init-outline.js
+++ init-outline.js
@@ -1,9 +1,9 @@
 // export default object with the initialization data
 
 export default {
-  mod1: 'Course intro, client requirements and RFPs, rapid response',
-  mod2: 'Team organization, work allocation, begin consolidated design',
-  mod3: 'Begin implementation of Minimally Viable Products (MVP)',
+  mod1: 'Course intro, client requirements and RFP, rapid response challenge',
+  mod2: 'App plan & design: Team organization, work allocation, consolidated detailed design documentation',
+  mod3: 'Implementation of Minimally Viable Products (MVP) begins',
   mod4: 'Demonstrations, updates to design and MVPs',
   mod5: 'Demonstrations, updates to design and MVPs',
   mod6: 'Demonstrations, updates to design and MVPs',
